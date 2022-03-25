b097d136aa0ad86b22b19fefe9142ca2e694fc17
Flávio Tonon <flavioltonon@gmail.com>
2022-03-25 18:30:59 -0300

    feat: create mock users and profiles when the server has DevelopmentMode enabled

--- infrastructure/drivers/drivers.go
+++ infrastructure/drivers/drivers.go
@@ -1,10 +1,13 @@
 package drivers
 
 import (
+	"flavioltonon/hmv/domain/entity"
+	"flavioltonon/hmv/domain/valueobject"
 	"flavioltonon/hmv/infrastructure/logging"
 	"flavioltonon/hmv/infrastructure/presenter"
 	"flavioltonon/hmv/infrastructure/repository"
 	"flavioltonon/hmv/infrastructure/settings"
+	"time"
 )
 
 // Drivers groups the application dependencies
@@ -19,6 +22,60 @@ func New(settings *settings.Settings) (*Drivers, error) {
 	repositories, err := repository.NewRepositories()
 	if err != nil {
 		return nil, err
+	}
+
+	if settings.Server.DevelopmentMode {
+		repositories.Users.CreateUser(&entity.User{
+			ID:          "af3ecfbc-0e18-4448-875a-d64744a1f5cd",
+			Username:    "paciente@teste.com",
+			Password:    "1234abc@",
+			ProfileKind: valueobject.Pacient_ProfileKind,
+			CreatedAt:   time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+			UpdatedAt:   time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+		})
+
+		repositories.Pacients.CreatePacient(&entity.Pacient{
+			ID:     "aed15ceb-c95e-4ee7-ae75-dd80e7f7da67",
+			UserID: "af3ecfbc-0e18-4448-875a-d64744a1f5cd",
+			EmergencyContact: valueobject.EmergencyContact{
+				Name:         "Contato de emergência",
+				MobileNumber: "5511999999999",
+			},
+			CreatedAt: time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+			UpdatedAt: time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+		})
+
+		repositories.Users.CreateUser(&entity.User{
+			ID:          "6d763219-d191-4970-85cf-5abeab25f9c4",
+			Username:    "analista@hmv.com.br",
+			Password:    "1234abc@",
+			ProfileKind: valueobject.Analyst_ProfileKind,
+			CreatedAt:   time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+			UpdatedAt:   time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+		})
+
+		repositories.Analysts.CreateAnalyst(&entity.Analyst{
+			ID:        "7470aade-d9b1-413e-8e76-d2eb837864ce",
+			UserID:    "6d763219-d191-4970-85cf-5abeab25f9c4",
+			CreatedAt: time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+			UpdatedAt: time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+		})
+
+		repositories.Users.CreateUser(&entity.User{
+			ID:          "16674382-3430-414e-b429-0e847999ca8e",
+			Username:    "socorrista@hmv.com.br",
+			Password:    "1234abc@",
+			ProfileKind: valueobject.Rescuer_ProfileKind,
+			CreatedAt:   time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+			UpdatedAt:   time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+		})
+
+		repositories.Rescuers.CreateRescuer(&entity.Rescuer{
+			ID:        "b6d41f8e-bf3c-451c-bc83-6f01312aed55",
+			UserID:    "16674382-3430-414e-b429-0e847999ca8e",
+			CreatedAt: time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+			UpdatedAt: time.Date(2022, 25, 03, 21, 18, 33, 0, time.UTC),
+		})
 	}
 
 	logger, err := logging.NewZapLogger(settings.Logging)
