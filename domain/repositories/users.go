451a13ee74c4d106d82a10066728672a20ea47b3
Flávio Tonon <flavioltonon@gmail.com>
2022-03-25 11:13:25 -0300

    docs: add comments to UsersRepository interface

--- domain/repositories/users.go
+++ domain/repositories/users.go
@@ -2,13 +2,24 @@ package repositories
 
 import "flavioltonon/hmv/domain/entity"
 
+// UsersRepository is a repository for entity.User entities
 type UsersRepository interface {
+	// CreateUser stores an entity.User into the repository. If an User with the same ID already
+	// exists in the repository, entity.ErrDuplicatedEntry should be returned instead.
 	CreateUser(user *entity.User) error
+
+	// FindUserByID returns an entity.User identified by a given userID. If no entities are found,
+	// entity.ErrNotFound should be returned instead.
 	FindUserByID(userID string) (*entity.User, error)
+
+	// FindUserByUsername returns an entity.User identified by a given username. If no entities are found,
+	// entity.ErrNotFound should be returned instead.
 	FindUserByUsername(username string) (*entity.User, error)
 
 	// ListUsers returns all the entity.User in the repository
 	ListUsers() ([]*entity.User, error)
 
+	// UpdateUser updates an User in the repository. If no entities with the same ID as the input entity.User are found,
+	// entity.ErrNotFound should be returned instead.
 	UpdateUser(user *entity.User) error
 }
