ff87a2d869b026a79a1e9c6739babf8c1c6d838e
Flávio Tonon <flavioltonon@gmail.com>
2022-03-27 14:45:08 -0300

    feat: implement endpoints to update pacients' health and location data

--- api/controller/pacients/controller_update_location_data.go
+++ api/controller/pacients/controller_update_location_data.go
@@ -16,7 +16,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func (c *Controller) updateEmergencyContact(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) updateLocationData(w http.ResponseWriter, r *http.Request) {
 	user, err := entity.NewUserFromRequest(r)
 	if err != nil {
 		c.drivers.Logger.Info(application.FailedToAuthenticateUser, logging.Error(err))
@@ -24,7 +24,7 @@ func (c *Controller) updateEmergencyContact(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	payload, err := c.newUpdateEmergencyContactPayload(r)
+	payload, err := c.newUpdateLocationDataPayload(r)
 	if err != nil {
 		c.drivers.Logger.Info(application.FailedToValidateRequest, logging.Error(err))
 		c.drivers.Presenter.Present(w, response.BadRequest(application.FailedToValidateRequest, err))
@@ -33,7 +33,7 @@ func (c *Controller) updateEmergencyContact(w http.ResponseWriter, r *http.Reque
 
 	vars := mux.Vars(r)
 
-	updatedPacient, err := c.usecases.Pacients.UpdateEmergencyContact(user.ID, vars["pacient_id"], payload.toValueObject())
+	updatedPacient, err := c.usecases.Pacients.UpdateLocationData(user.ID, vars["pacient_id"], payload.toValueObject())
 	if errors.Is(err, entity.ErrNotFound) {
 		c.drivers.Logger.Info(application.FailedToUpdatePacient, logging.Error(err))
 		c.drivers.Presenter.Present(w, response.BadRequest(application.FailedToUpdatePacient, err))
@@ -55,27 +55,33 @@ func (c *Controller) updateEmergencyContact(w http.ResponseWriter, r *http.Reque
 	c.drivers.Presenter.Present(w, response.Created(presenter.NewPacient(updatedPacient)))
 }
 
-type updateEmergencyContactPayload struct {
-	Name         string `json:"name"`
-	MobileNumber string `json:"mobile_number"`
+type updateLocationDataPayload struct {
+	State   string `json:"state"`
+	City    string `json:"city"`
+	Address string `json:"address"`
+	ZipCode string `json:"zipcode"`
 }
 
-func (p *updateEmergencyContactPayload) Validate() error {
+func (p *updateLocationDataPayload) Validate() error {
 	return ozzo.ValidateStruct(p,
-		ozzo.Field(&p.Name, ozzo.Required),
-		ozzo.Field(&p.MobileNumber, ozzo.Required),
+		ozzo.Field(&p.State, ozzo.Required),
+		ozzo.Field(&p.City, ozzo.Required),
+		ozzo.Field(&p.Address, ozzo.Required),
+		ozzo.Field(&p.ZipCode, ozzo.Required),
 	)
 }
 
-func (p *updateEmergencyContactPayload) toValueObject() valueobject.EmergencyContact {
-	return valueobject.EmergencyContact{
-		Name:         p.Name,
-		MobileNumber: p.MobileNumber,
+func (p *updateLocationDataPayload) toValueObject() valueobject.LocationData {
+	return valueobject.LocationData{
+		State:   p.State,
+		City:    p.City,
+		Address: p.Address,
+		ZipCode: p.ZipCode,
 	}
 }
 
-func (c *Controller) newUpdateEmergencyContactPayload(r *http.Request) (*updateEmergencyContactPayload, error) {
-	var payload updateEmergencyContactPayload
+func (c *Controller) newUpdateLocationDataPayload(r *http.Request) (*updateLocationDataPayload, error) {
+	var payload updateLocationDataPayload
 
 	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
 		return nil, err
